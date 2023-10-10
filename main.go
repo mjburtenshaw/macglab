eddac5c2702452984246808af2d941995e0a71f1
Malcolm Burtenshaw <malcolm@gosolo.io>
2023-10-10 11:49:21 -0600

    refactor: main package
    Error Handling:
    Replaced panic with log.Fatalf for error logging, which is more idiomatic and provides a clearer indication of errors.
    Added error checking to mrs.OpenMergeRequests(allMrs) to handle any errors that occur when attempting to open merge requests in the browser.
    Function Decomposition:
    Extracted the logic for fetching merge requests into a separate fetchMergeRequests function to simplify the main function and promote single responsibility.
    Naming:
    The flag variable names were left unchanged as they are descriptive. However, you may consider renaming them to be more aligned with Go's naming conventions if necessary.
    Logging:
    Used log.Printf to log errors instead of causing a panic, which allows for a more graceful handling of errors.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"log"
 
 	"github.com/mjburtenshaw/macglab/config"
 	"github.com/mjburtenshaw/macglab/mrs"
@@ -14,15 +15,29 @@ func main() {
 	groupFlag := flag.Bool("group", false, "Filter output to the usernames configuration.")
 	projectsFlag := flag.Bool("projects", false, "Filter output to the projects configuration.")
 
-
 	flag.Parse()
 
+	allMrs, err := fetchMergeRequests(draftFlag, groupFlag, projectsFlag)
+	if err != nil {
+		log.Fatalf("Failed to fetch merge requests: %v", err)
+	}
+
+	mrs.PrintMergeRequests(allMrs)
+
+	if *browserFlag {
+		if err := mrs.OpenMergeRequests(allMrs); err != nil {
+			log.Printf("Failed to open merge requests in browser: %v", err)
+		}
+	}
+}
+
+func fetchMergeRequests(draftFlag, groupFlag, projectsFlag *bool) ([]*gitlab.MergeRequest, error) {
 	var allMrs []*gitlab.MergeRequest
 
 	if (!*groupFlag && !*projectsFlag) || *groupFlag {
 		groupMrs, err := mrs.FetchGroupMergeRequests(draftFlag)
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 		allMrs = append(allMrs, groupMrs...)
 	}
@@ -31,20 +46,16 @@ func main() {
 		allUsernames := config.Projects["all"]
 
 		for project, usernames := range config.Projects {
-				if project != "all" {
-						combinedUsernames := append(usernames, allUsernames...)
-						projectMrs, err := mrs.FetchProjectMergeRequests(project, combinedUsernames, draftFlag)
-						if err != nil {
-							panic(err)
-						}
-						allMrs = append(allMrs, projectMrs...)
+			if project != "all" {
+				combinedUsernames := append(usernames, allUsernames...)
+				projectMrs, err := mrs.FetchProjectMergeRequests(project, combinedUsernames, draftFlag)
+				if err != nil {
+					return nil, err
 				}
+				allMrs = append(allMrs, projectMrs...)
+			}
 		}
 	}
 
-	mrs.PrintMergeRequests(allMrs)
-
-	if *browserFlag {
-		mrs.OpenMergeRequests(allMrs)
-	}
+	return allMrs, nil
 }
