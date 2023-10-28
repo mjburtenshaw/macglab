812550e8482df9253124af498003c57cfbf33751
Malcolm Burtenshaw <malcolm@gosolo.io>
2023-10-27 23:39:09 -0600

    refactor: Reduce API surface

--- config/config.go
+++ config/config.go
@@ -8,7 +8,7 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-type Config struct {
+type TConfig struct {
 	AccessToken string              `yaml:"ACCESS_TOKEN"`
 	GroupId     string              `yaml:"GROUP_ID"`
 	Me          int                 `yaml:"ME"`
@@ -16,70 +16,63 @@ type Config struct {
 	Usernames   []string            `yaml:"USERNAMES"`
 }
 
-var (
-	config      *Config
-	AccessToken string
-	GroupId     string
-	Me          int
-	Projects    map[string][]string
-	Usernames   []string
-)
+var Config *TConfig
 
-func GetConfig(configUrl string) (*Config, error) {
-    if err := CheckFileExists(configUrl); err != nil {
-        return nil, fmt.Errorf("couldn't find %s: %w", configUrl, err)
-    }
+func GetConfig(configUrl string) (*TConfig, error) {
+	if err := CheckFileExists(configUrl); err != nil {
+		return nil, fmt.Errorf("couldn't find %s: %w", configUrl, err)
+	}
 
 	configFile, err := os.ReadFile(configUrl)
 	if err != nil {
 		return nil, fmt.Errorf("couldn't read %s: %w", configUrl, err)
 	}
 
-	if err = yaml.Unmarshal(configFile, &config); err != nil {
-        return nil, fmt.Errorf("couldn't unmarshal %s: %w", configUrl, err)
-    }
+	if err = yaml.Unmarshal(configFile, &Config); err != nil {
+		return nil, fmt.Errorf("couldn't unmarshal %s: %w", configUrl, err)
+	}
 
-    return config, nil
+	return Config, nil
 }
 
 func AddConfig(sampleConfigUrl string, configUrl string) (err error) {
-    sampleConfig, err := os.Open(sampleConfigUrl)
-    if err != nil {
-        return fmt.Errorf("couldn't open sample config: %s", err)
-    }
-    defer func() {
-        if cerr := sampleConfig.Close(); cerr != nil && err == nil {
-            err = fmt.Errorf("couldn't close sample config: %s", cerr)
-        }
-    }()
+	sampleConfig, err := os.Open(sampleConfigUrl)
+	if err != nil {
+		return fmt.Errorf("couldn't open sample config: %s", err)
+	}
+	defer func() {
+		if cerr := sampleConfig.Close(); cerr != nil && err == nil {
+			err = fmt.Errorf("couldn't close sample config: %s", cerr)
+		}
+	}()
 
-    configFile, err := os.Create(configUrl)
-    if err != nil {
-        return fmt.Errorf("couldn't create config: %s", err)
-    }
-    defer func() {
-        if cerr := configFile.Close(); cerr != nil && err == nil {
-            err = fmt.Errorf("couldn't close config: %s", cerr)
-        }
-    }()
+	configFile, err := os.Create(configUrl)
+	if err != nil {
+		return fmt.Errorf("couldn't create config: %s", err)
+	}
+	defer func() {
+		if cerr := configFile.Close(); cerr != nil && err == nil {
+			err = fmt.Errorf("couldn't close config: %s", cerr)
+		}
+	}()
 
-    if _, err = io.Copy(configFile, sampleConfig); err != nil {
-        return fmt.Errorf("couldn't copy config: %s", err)
-    }
+	if _, err = io.Copy(configFile, sampleConfig); err != nil {
+		return fmt.Errorf("couldn't copy config: %s", err)
+	}
 
-    return nil
+	return nil
 }
 
 func ChangeConfig(configUrl string, key string, nextValue string) (err error) {
 	configFile, err := os.OpenFile(configUrl, os.O_RDWR, 0)
-    if err != nil {
-        return fmt.Errorf("couldn't open config file: %w", err)
-    }
+	if err != nil {
+		return fmt.Errorf("couldn't open config file: %w", err)
+	}
 	defer func() {
-        if cerr := configFile.Close(); cerr != nil && err == nil {
-            err = fmt.Errorf("couldn't close config file: %s", cerr)
-        }
-    }()
+		if cerr := configFile.Close(); cerr != nil && err == nil {
+			err = fmt.Errorf("couldn't close config file: %s", cerr)
+		}
+	}()
 
 	data, err := io.ReadAll(configFile)
 	if err != nil {
@@ -88,23 +81,23 @@ func ChangeConfig(configUrl string, key string, nextValue string) (err error) {
 
 	var content map[interface{}]interface{}
 	if err = yaml.Unmarshal(data, &content); err != nil {
-        return fmt.Errorf("couldn't unmarshal config file: %w", err)
+		return fmt.Errorf("couldn't unmarshal config file: %w", err)
 	}
 
 	if _, ok := content[key]; ok {
 		content[key] = nextValue
 	} else {
-        return fmt.Errorf("invalid key: %s", key)
+		return fmt.Errorf("invalid key: %s", key)
 	}
 
 	output, err := yaml.Marshal(content)
 	if err != nil {
-        return fmt.Errorf("couldn't marshal config file: %w", err)
+		return fmt.Errorf("couldn't marshal config file: %w", err)
 	}
 
 	if err = os.WriteFile(configUrl, output, 0644); err != nil {
 		return fmt.Errorf("couldn't update config file: %w", err)
 	}
 
-    return nil
+	return nil
 }
