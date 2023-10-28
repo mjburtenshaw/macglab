75047688ec8d12752dab640ed6563b70bfad886a
Malcolm Burtenshaw <malcolm@gosolo.io>
2023-10-27 22:07:37 -0600

    refactor: Remove fmt refs

--- cmd/init.go
+++ cmd/init.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -19,11 +18,11 @@ var initCmd = &cobra.Command{
 	Short: "Initializes macglab",
 	Long:  `Initializes macglab`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("macglab: 🏗️  Installing macglab...")
+		log.Println("macglab: installing macglab...")
 
 		isNewInstall := false
 		if _, err := os.Stat(config.MacglabUri); os.IsNotExist(err) {
-			fmt.Println("macglab: 🆕 No previous installation detected. *cracks knuckles* Starting from scratch...")
+			log.Println("macglab: no previous installation detected. *cracks knuckles* Starting from scratch...")
 			isNewInstall = true
 
 			log.Println("macglab: demanding a home directory for macglab...")
@@ -31,26 +30,26 @@ var initCmd = &cobra.Command{
 				log.Fatalf("macglab: couldn't create macglab config directory: %s", err)
 			}
 
-			fmt.Println("macglab: adding environment variables...")
+			log.Println("macglab: adding environment variables...")
 			if err := config.AddEnv(config.ShConfigUrl); err != nil {
 				log.Fatalf("macglab: couldn't add environment variables: %s", err)
 			}
 		}
 
 		if isNewInstall {
-			fmt.Println("macglab: 📜 Making a new config file...")
+			log.Println("macglab: making a new config file...")
 			cmd := exec.Command("cp", "config.sample.yml", config.MacglabConfigUrl)
 			err := cmd.Run()
 			if err != nil {
-				log.Fatal("macglab: 💀 Couldn't create macglab config file.")
+				log.Fatalf("macglab: couldn't create macglab config file: %s", err)
 			}
 		}
 
-		fmt.Println("macglab: 🎉 Successfully installed!")
+		log.Println("macglab: 🎉 Successfully installed!")
 
 		if isNewInstall {
-			fmt.Printf("macglab: 📜 Created a new config file at %s. Please open it and define values.\n", config.MacglabConfigUrl)
-			fmt.Println("macglab: 🐚 Re-source your shell session or open a new terminal, then run `macglab list` and watch the magic happen!")
+			log.Printf("macglab: created a new config file at %s. Please open it and define values.\n", config.MacglabConfigUrl)
+			log.Println("macglab: re-source your shell session or open a new terminal, then run `macglab list` and watch the magic happen!")
 		}
 	},
 }
