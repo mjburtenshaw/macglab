ac4a56badb551367884692280cb2a7536e463690
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2023-11-11 21:57:58 -0700

    style: Format document

--- cmd/root.go
+++ cmd/root.go
@@ -12,13 +12,13 @@ var rootCmd = &cobra.Command{
 	Short: "macglab automates gathering your work on gitlab.com to save time.",
 	Long: `macglab automates gathering your work on gitlab.com to save time.
 
-          This program lists all GitLab Merge Requests (MRs) based on:
-          
-          - Open state
-          - Specified usernames and/or projects
-          - Specified group
-          
-          Complete documentation is available at https://github.com/mjburtenshaw/macglab`,
+        This program lists all GitLab Merge Requests (MRs) based on:
+        
+        - Open state
+        - Specified usernames and/or projects
+        - Specified group
+        
+        Complete documentation is available at https://github.com/mjburtenshaw/macglab`,
 	Run: func(cmd *cobra.Command, args []string) {
 	},
 }
