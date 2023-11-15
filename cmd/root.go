a597d70759f593295a87010553f01429ac69e444
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2023-11-14 19:51:54 -0700

    docs: Update to v4.5.1

--- cmd/root.go
+++ cmd/root.go
@@ -18,7 +18,7 @@ Complete documentation is available at https://github.com/mjburtenshaw/macglab
 `
 
 var rootCmd = &cobra.Command{
-	Version: "4.5.0",
+	Version: "4.5.1",
 	Use:   "macglab",
 	Short: "macglab automates gathering your work on gitlab.com to save time.",
 	Long: help,
